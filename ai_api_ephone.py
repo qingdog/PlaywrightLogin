ae04088c8cf43ca7757dd8498f2ec3635f9de448
qingdog <69775024+qingdog@users.noreply.github.com>
2025-05-23 14:24:23 +0800

    Update ai_api_ephone.py

--- ai_api_ephone.py
+++ ai_api_ephone.py
@@ -60,20 +60,20 @@ def run(playwright: Playwright) -> None:
             page.get_by_role("button", name=" 去签到").click()
             js = "document.title"
             slide_validate.validate(page,page_url=None, page_evaluate=js, background_css="img.gocaptcha-module_picture__LRwbY", slider_css="div.index-module_tile__8pkQD img", background_size=(300, 220), slider_down_css_xpath="div.gocaptcha-module_dragBlock__bFlwx", distance_correction=-12)
-            
-            
         except TimeoutError as e:
             check_button = True
             raise e
-        try: page.wait_for_load_state(state="load", timeout=1000)  # 最长只等待1s，不管是否load完成，就进行下一步
-        except Exception as e: logging.error(e, exc_info=True)
-
-        alert_success_locator = page.locator('div[role="alert"][aria-label="success type"]')
-        # 断言为签到成功
+            
+        # 断言为成功
         try: expect(page.locator("span.semi-toast-content-text").last).to_contain_text("验证成功")
         except Exception as e: logging.error(e, exc_info=True)
+        
+        try: page.wait_for_load_state(state="load", timeout=1000)  # 最长只等待1s，不管是否load完成，就进行下一步
+        except Exception as e: logging.error(e, exc_info=True)
+        
         page.get_by_role("button", name=" 去签到").click()
         
+        alert_success_locator = page.locator('div[role="alert"][aria-label="success type"]')
         expect(alert_success_locator.last).to_contain_text("签到成功")
 
         # 疑似在断言时间里，产生了等待1s的行为（导致前面打印失败了，但断言成功了）。这里进行重新打印
